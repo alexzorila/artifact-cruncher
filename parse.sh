a283f6051e4a41f6e8a7a976ed6dfe8c2153cfb8
alexzorila <alexandru.zorila@gmail.com>
2024-07-31 22:43:13 +0100

    Add files via upload

---
+++ parse.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Require elevation
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+# Check if script has at least one argument
+if [[ $# -lt 2 ]] ; then
+    echo -e "\nNo parameter passed!\n\tPlease use -f or --filename followed by a full path.\n\tEg: parse.sh -f DESKTOP-123.zip\n"
+    exit 1
+fi
+
+# Read script arguments to store filename to be parsed
+while [[ $# -gt 0 ]]
+do case $1 in
+    -f|--filename) filename="$2"
+    shift;;
+    *) echo -e "\nUnknown parameter passed: $1\n\tPlease use -f or --filename followed by a full path.\n\tEg: parse.sh -f DESKTOP-123.zip\n"
+    exit 1;;
+esac
+shift
+done
+
+# Check if filename exists
+if [ ! -f $filename ]; then
+    echo -e "\nFile $filename not found!\nExiting..\n"
+    exit 1
+else
+    echo -e "\nFile to be parsed: $filename"
+fi
+
+# Confirm user choice
+read -p "Continue (y/y)?" choice
+case "$choice" in 
+  y|Y)
+  	# File naming and working directory setup
+	basename=$(basename $filename .zip)
+	datetime=$(date -d "today" +"%Y%m%d%H%M")
+	originalwd=$PWD
+	workdir="/tmp/$datetime"
+	mkdir $workdir
+	cp $filename $workdir
+	cd $workdir
+	
+	
+	### Parse Collection to Plaso ###	
+	# Extract MFT
+	unzip -o -j $filename */\$MFT -d $workdir
+	
+	# Parse MFT to body file
+	MFTECmd -f $workdir/\$MFT --body $workdir --bodyf $basename.mft.body --bdl c
+	
+	# Parse Triage image to Plaso file, excluding MFT
+	docker run -v .:/data log2timeline/plaso \
+		log2timeline --parsers \!mft \
+		--storage_file /data/$basename.plaso /data/$filename
+	
+	# Merge MFT body file with Triage image Plaso
+	docker run -v .:/data log2timeline/plaso \
+		log2timeline -z UTC --parsers mactime \
+		--storage_file /data/$basename.plaso /data/$basename.mft.body
+	
+	
+	# Retrieve parsed data
+	mv -f $workdir/$basename.plaso $originalwd
+	
+	# Clean working directory
+	rm -rf $workdir
+	exit 0
+  ;;
+  n|N) echo -e "No selected. Exiting.\n"
+	exit 1
+  ;;
+  *) echo -e "Invalid choice. Please answer y or n.\n"
+	exit 1
+  ;;
+esac
---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Elevate session
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+# Install dependencies
+echo -e "\n[$(date '+%d/%m/%Y %H:%M:%S')]: Installing dependencies."
+apt update -y
+apt install dotnet6 unzip -y
+
+# Install MFTECmd
+echo -e "\n\n[$(date '+%d/%m/%Y %H:%M:%S')]: Installing MFTECmd."
+git clone https://github.com/EricZimmerman/MFTECmd.git /opt/MFTECmd
+dotnet publish /opt/MFTECmd/ -r ubuntu.22.04-x64 --self-contained --framework net6.0
+cp -s /opt/MFTECmd/MFTECmd/bin/Debug/net6.0/ubuntu.22.04-x64/MFTECmd /usr/local/bin/
+
+# Install Docker
+echo -e "\n\n[$(date '+%d/%m/%Y %H:%M:%S')]: Installing Docker."
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+# Install Plaso
+echo -e "\n\n[$(date '+%d/%m/%Y %H:%M:%S')]: Installing Plaso."
+docker pull log2timeline/plaso
+
+# Install parsing script
+echo -e "\n\n[$(date '+%d/%m/%Y %H:%M:%S')]: Installing Parsing Script."
+mkdir /opt/parse
+cp ./parse.sh /opt/parse/parse
+cp -s /opt/parse/parse /usr/local/bin/
+
+# Cleanup
+rm -rf get-docker.sh
+echo -e "\n\n[$(date '+%d/%m/%Y %H:%M:%S')]: Setup completed.\n"
