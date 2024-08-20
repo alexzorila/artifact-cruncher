10f18a9660205673efe29b614be88b24bb8c77c5
alexzorila <alexandru.zorila@gmail.com>
2024-08-20 04:24:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
+# Elevate session
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
 # Flag to check if a valid argument was provided
 valid_arg=false
 
 # Execute setup scripts based on provided arguments
 for arg in "$@"; do
     case "$arg" in
-        parse) echo -e "\nExecuting ./parse/setup.sh..."; cd parse && ./setup.sh; valid_arg=true ;;
-	splunk) echo -e "\nExecuting ./splunk/setup.sh..."; cd splunk && ./setup.sh; valid_arg=true ;;
+        parse) 	
+			echo -e "\nExecuting ./parse/setup.sh..."
+			cd parse; ./setup.sh; cd ..; valid_arg=true 
+			;;
+		splunk) 
+			echo -e "\nExecuting ./splunk/setup.sh..."
+			cd splunk; ./setup.sh; cd ..; valid_arg=true 
+			;;
     esac
 done
 
 # Notify the user if no valid keyword was provided
 if [ "$valid_arg" = false ]; then
-    echo -e "\n\tNo valid parameter passed!"
-	echo -e "\tUse 'parse' or 'splunk' to install one or both."
-	echo -e "\n\tExamples:"
-	echo -e "\t- Install both: ./setup.sh parse splunk"
-	echo -e "\t- Install parser: ./setup.sh parse"
-	echo -e "\t- Install splunk: ./setup.sh splunk\n"
+    echo -e "\nError   : No valid script arguments passed! Specify one or both to install corresponding module."
+	echo -e "Options : 'parse', 'splunk'"
+	echo -e "\nExamples:"
+	echo -e "\t- Install both   : ./setup.sh parse splunk"
+	echo -e "\t- Install parser : ./setup.sh parse"
+	echo -e "\t- Install splunk : ./setup.sh splunk\n"
     exit 1
 fi
-echo -e "\nDone!\n"
+echo -e "\nSetup Done!\n"
