d78206cdf71f73472b5df3fbe5c3820e3ac1f300
alexzorila <alexandru.zorila@gmail.com>
2024-08-20 03:36:38 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Flag to check if a valid argument was provided
+valid_arg=false
+
+# Execute setup scripts based on provided arguments
+for arg in "$@"; do
+    case "$arg" in
+        parse) echo -e "\nExecuting ./parse/setup.sh..."; ./parse/setup.sh; valid_arg=true ;;
+		splunk) echo -e "\nExecuting ./splunk/setup.sh..."; ./splunk/setup.sh; valid_arg=true ;;
+    esac
+done
+
+# Notify the user if no valid keyword was provided
+if [ "$valid_arg" = false ]; then
+    echo -e "\n\tNo valid parameter passed!"
+	echo -e "\tUse 'parse' or 'splunk' to install one or both."
+	echo -e "\n\tExamples:"
+	echo -e "\t- Install both: ./setup.sh parse splunk"
+	echo -e "\t- Install parser: ./setup.sh parse"
+	echo -e "\t- Install splunk: ./setup.sh splunk\n"
+    exit 1
+fi
+echo -e "\nDone!\n"
