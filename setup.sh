800998324a3f9760f6fee4b8fc5f96e525e7e538
alexzorila <alexandru.zorila@gmail.com>
2024-08-20 04:33:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,13 +13,13 @@ valid_arg=false
 for arg in "$@"; do
     case "$arg" in
         parse) 	
-			echo -e "\nExecuting ./parse/setup.sh..."
-			cd parse; ./setup.sh; cd ..; valid_arg=true 
-			;;
-		splunk) 
-			echo -e "\nExecuting ./splunk/setup.sh..."
-			cd splunk; ./setup.sh; cd ..; valid_arg=true 
-			;;
+	        echo -e "\nExecuting ./parse/setup.sh..."
+		cd parse; ./setup.sh; cd ..; valid_arg=true 
+		;;
+	splunk) 
+		echo -e "\nExecuting ./splunk/setup.sh..."
+		cd splunk; ./setup.sh; cd ..; valid_arg=true 
+		;;
     esac
 done
 
