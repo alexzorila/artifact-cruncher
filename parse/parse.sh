258151bb92e1e4dc4b6af93c19adf846f556ffb1
alexzorila <alexandru.zorila@gmail.com>
2024-08-26 04:15:10 +0100

    Update parse.sh

--- parse/parse.sh
+++ parse/parse.sh
@@ -36,57 +36,57 @@ read -p "Continue (y/y)? " choice
 case "$choice" in
   y|Y)
         # Set up work directory
-		hostname=$(echo $filename | grep -oP '(?<=Collection-).*(?=-\d{4}-\d{2}-\d{2}T\d{2}_\d{2}_\d{2}Z)')
-		datetime=$(date -d "today" +"%Y%m%d%H%M")
+        hostname=$(echo $filename | grep -oP '(?<=Collection-).*(?=-\d{4}-\d{2}-\d{2}T\d{2}_\d{2}_\d{2}Z)')
+        datetime=$(date -d "today" +"%Y%m%d%H%M")
         workdir="/tmp/$hostname/$datetime"
         hostdir=$PWD/$hostname/$datetime
         mkdir -p $workdir $hostdir
-		cp $filename $workdir
-		cd $workdir
-		
+        cp $filename $workdir
+        cd $workdir
+        
         ########################## Create Timeline ############################
         
-		# Create MFT timeline work directory
+        # Create MFT timeline work directory
         mkdir mft
-		
-		# Get MFT file names from collection
+        
+        # Get MFT file names from collection
         mftfiles=$(unzip -l $filename | grep -i mft | awk '{print $4}')
         
-		# Parse each MFT file in the list to CSV Timeline
-        for mft in $mftfiles; do    
+        # Parse each MFT file in the list to CSV Timeline
+        for mft in $mftfiles; do
             # Get drive letter
-            drive=$(echo $mft | grep -oP '(?<=%5C%5C.%5C).*(?=%3A/\$MFT)')         
+            drive=$(echo $mft | grep -oP '(?<=%5C%5C.%5C).*(?=%3A/\$MFT)')
             # Extract MFT
-            unzip -o -j $filename $mft -d .         
+            unzip -o -j $filename $mft -d .
             # Parse MFT to body file
             MFTECmd -f \$MFT --body mft --bodyf $drive.mft.body --bdl $drive
-			rm -rf \$MFT           
+            rm -rf \$MFT
             # Parse body file to CSV Timeline
-            mactime -b mft/$drive.mft.body -d -y -z UTC > mft/$drive.MftTimeline.csv     		
-        done        
+            mactime -b mft/$drive.mft.body -d -y -z UTC > mft/$drive.MftTimeline.csv
+        done
         
-		# Merge CSV Timeline
+        # Merge CSV Timeline
         awk 'NR == 1 || FNR > 1' mft/*.csv > MftTimeline.csv  
         
-		# Move CSV to host
+        # Move CSV to host
         mv -f MftTimeline.csv "$hostdir" && rm -rf mft
 
 
         ######################### Create Supertimeline ########################
 
         # Create Plaso data source directory
-		mkdir files	
-		
-		# Expand triage collection and remove extra files
+        mkdir files
+        
+        # Expand triage collection and remove extra files
         unzip $filename -d exp && mv ./exp/uploads/* ./files && rm -rf exp $filename
         
         # Parse triage collection to CSV Supertimeline, excluding MFT
         docker run --rm -v .:/data log2timeline/plaso \
-			psteal --parsers \!mft --hashers none --archives none \
+            psteal --parsers \!mft --hashers none --archives none \
                    --source /data/files -w /data/Supertimeline.csv
             
         # Move CSV to host
-        mv -f Supertimeline.csv "$hostdir" && rm -rf $workdir		
+        mv -f Supertimeline.csv "$hostdir" && rm -rf $workdir
         exit 0
   ;;
   n|N) echo -e "No selected. Exiting.\n"
