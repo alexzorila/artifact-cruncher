b6fa2dd60662e0c398e54753d65e01ec50dcea5e
alexzorila <alexandru.zorila@gmail.com>
2025-01-14 02:39:50 +0000

    Add files via upload

---
+++ collect/build.cmd
@@ -0,0 +1,12 @@
+
+:: Get Velociraptor
+powershell "$ProgressPreference = 'SilentlyContinue'; curl https://github.com/Velocidex/velociraptor/releases/download/v0.73/velociraptor-v0.73.3-windows-amd64.exe -o .\bin\velociraptor.exe"
+
+:: Build Velociraptor Offline collector from spec file
+.\bin\velociraptor collector .\conf\spec_file.yaml
+
+:: Copy collector to current dir
+copy /y %TMP%\gui_datastore\collect.exe .\collect.exe
+
+:: Prevent window auto close
+pause
