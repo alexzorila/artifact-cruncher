fe05de8704b89caa9c787f6036738e7606dd3135
alexzorila <alexandru.zorila@gmail.com>
2025-09-27 03:00:52 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,25 +12,25 @@ valid_arg=false
 # Execute setup scripts based on provided arguments
 for arg in "$@"; do
     case "$arg" in
-        parse) 	
-		echo -e "\nExecuting ./parse/setup.sh..."
-		cd parse; chmod +x ./setup.sh; ./setup.sh; cd ..; valid_arg=true 
-		;;
-	splunk) 
-		echo -e "\nExecuting ./splunk/setup.sh..."
-		cd splunk; chmod +x ./setup.sh; ./setup.sh; cd ..; valid_arg=true 
-		;;
+    parse)     
+        echo -e "\nExecuting ./parse/setup.sh..."
+        cd parse; chmod +x ./setup.sh; ./setup.sh; cd ..; valid_arg=true 
+        ;;
+    splunk) 
+        echo -e "\nExecuting ./splunk/setup.sh..."
+        cd splunk; chmod +x ./setup.sh; ./setup.sh; cd ..; valid_arg=true 
+        ;;
     esac
 done
 
 # Help page. Notify user if no valid argument provided
 if [ "$valid_arg" = false ]; then
     echo -e "\nError   : No valid script arguments passed! Specify one or both to install corresponding module."
-	echo -e "Options : 'parse', 'splunk'"
-	echo -e "\nExamples:"
-	echo -e "\t- Install both   : ./setup.sh parse splunk"
-	echo -e "\t- Install parser : ./setup.sh parse"
-	echo -e "\t- Install splunk : ./setup.sh splunk\n"
+    echo -e "Options : 'parse', 'splunk'"
+    echo -e "\nExamples:"
+    echo -e "\t- Install both   : ./setup.sh parse splunk"
+    echo -e "\t- Install parser : ./setup.sh parse"
+    echo -e "\t- Install splunk : ./setup.sh splunk\n"
     exit 1
 fi
 
